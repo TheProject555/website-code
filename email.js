181a157c8823666f851b1216660f588f42c58257
DawnCrawler01 <56701947+DawnCrawler01@users.noreply.github.com>
2020-04-09 11:09:53 -0400

    Update email.js

--- email.js
+++ email.js
@@ -4,14 +4,14 @@
 import {sendWithService} from 'backend/sendGrid';
 
 export function sendEmail(subject, body) {
-  const key = "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";
-  const sender = "english4life@gmail.com";
-  const recipient = "mmsconyers@liberty.edu";
+  const key = "Put API key here";
+  const sender = "sender@example.com";
+  const recipient = "recipient@example.com";
   return sendWithService(key, sender, recipient, subject, body);
 }
 
 export function sendEmailWithRecipient(subject, body, recipient) {
-  const key = "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";
-  const sender = "english4life@gmail.com";
+  const key = "Put API key here";
+  const sender = "sender@example.com";
   return sendWithService(key, sender, recipient, subject, body);
 }
